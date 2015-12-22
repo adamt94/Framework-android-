2c671aad5fa4704bcab85143a71804770d9e99d0
Ben Lang <bl3rune@yahoo.co.uk>
2015-12-22 15:01:47 +0000

    Notification done

--- framework/src/main/java/se2/framework/Notification.java
+++ framework/src/main/java/se2/framework/Notification.java
@@ -4,4 +4,22 @@ package se2.framework;
  * Created by BEN on 17/12/2015.
  */
 public interface Notification {
+
+    public void setSmallIcon(Image icon);
+
+    public void setContentTitle(String content);
+
+    public void setContentText(String text);
+
+    public Image setSmallIcon();
+
+    public String setContentTitle();
+
+    public String setContentText();
+
+    public void cancel();
+
+    public void cancelAll();
+
+    public void setAutoCancel();
 }
