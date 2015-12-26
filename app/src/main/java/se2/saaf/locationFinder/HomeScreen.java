bb26110b2fd7fc2096ab2b34569f958c30c15be6
CallumW1 <callum_wells@live.co.uk>
2015-12-26 20:10:13 +0000

    no message

--- app/src/main/java/se2/saaf/locationFinder/HomeScreen.java
+++ app/src/main/java/se2/saaf/locationFinder/HomeScreen.java
@@ -1,4 +1,4 @@
-package se2.saaf;
+package se2.saaf.locationFinder;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import se2.saaf.R;
 
 public class HomeScreen extends AppCompatActivity {
 
