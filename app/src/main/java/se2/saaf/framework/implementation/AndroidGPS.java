c4c64294490dde3a5d44db02cb826c2d7b23c6ab
Ben Lang <ben.lang@uea.ac.uk>
2016-01-09 17:02:35 +0000

    GPS patched

--- app/src/main/java/se2/saaf/framework/implementation/AndroidGPS.java
+++ app/src/main/java/se2/saaf/framework/implementation/AndroidGPS.java
@@ -16,64 +16,55 @@ import se2.saaf.framework.GPS;
  * Created by BEN on 17/12/2015.
  */
 public class AndroidGPS implements GPS {
-    private boolean isGPSEnabled = false;
-    private boolean isNetworkEnabled = false;
+    private boolean GPS = false;
+    private boolean Network = false;
     private LocationManager locationManager;
-    private LocationListener locationListener;
-    private Location location;
-    private double latitude;
-    private double longitude;
+    private AndroidLocationListener locationListener;
+    private Activity activity;
 
     public AndroidGPS(Activity activity) {
         // Acquire a reference to the system Location Manager
         locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
-
-        // Define a listener that responds to location updates
-        locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
-
-            public void onProviderEnabled(String provider) {
-            }
-
-            public void onProviderDisabled(String provider) {
-            }
-        };
-
-        // Register the listener with the Location Manager to receive location updates
-        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-        }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-        getLocation();
     }
 
-    @Override
-    public void onLocationChanged() {
+    private class AndroidLocationListener implements LocationListener
+    {
 
+        Location myLocation;
+        @Override
+        public void onLocationChanged(Location location) {
+            myLocation = location;
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onProviderDisabled(String provider) {}
     }
 
     @Override
     public Location getLocation() {
+
+        if (GPS || Network)
+        {
+            return locationListener.myLocation;
+        }
         return null;
     }
 
     @Override
-    public double getLongitude() {
-        return 0;
+    public void enableLocation() {
+
     }
 
     @Override
-    public double getLatitude() {
-        return 0;
+    public void disableLocation() {
+
     }
+
+
 }
